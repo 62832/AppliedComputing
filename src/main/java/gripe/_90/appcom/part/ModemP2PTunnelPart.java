06bcfe20f1c92f9d0e8968b9dd8caa4ff237fff0
90 <90@national.shitposting.agency>
2022-10-28 15:56:24 +0100

    Swap out wrong capability and interfaces

--- src/main/java/gripe/_90/appcom/part/ModemP2PTunnelPart.java
+++ src/main/java/gripe/_90/appcom/part/ModemP2PTunnelPart.java
@@ -3,16 +3,16 @@ package gripe._90.appcom.part;
 import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 import dan200.computercraft.api.network.IPacketReceiver;
+import dan200.computercraft.api.network.IPacketSender;
 import dan200.computercraft.api.network.Packet;
-import dan200.computercraft.api.network.wired.IWiredElement;
-import dan200.computercraft.api.network.wired.IWiredNode;
+import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.shared.Capabilities;
-import dan200.computercraft.shared.wired.WiredNode;
 
 import appeng.api.parts.IPartItem;
 import appeng.api.parts.IPartModel;
@@ -22,14 +22,16 @@ import appeng.parts.p2p.P2PModels;
 
 import gripe._90.appcom.AppliedComputing;
 
-public class ModemP2PTunnelPart extends CapabilityP2PTunnelPart<ModemP2PTunnelPart, IWiredElement> {
+public class ModemP2PTunnelPart extends CapabilityP2PTunnelPart<ModemP2PTunnelPart, IPeripheral> {
 
     private static final P2PModels MODELS = new P2PModels(AppliedComputing.makeId("part/modem_p2p_tunnel"));
+    private static final NullPacketHandler NULL_MODEM = new NullPacketHandler();
 
     public ModemP2PTunnelPart(IPartItem<?> partItem) {
-        super(partItem, Capabilities.CAPABILITY_WIRED_ELEMENT);
+        super(partItem, Capabilities.CAPABILITY_PERIPHERAL);
         inputHandler = new InputPacketHandler();
         outputHandler = new OutputPacketHandler();
+        emptyHandler = NULL_MODEM;
     }
 
     @PartModels
@@ -42,10 +44,55 @@ public class ModemP2PTunnelPart extends CapabilityP2PTunnelPart<ModemP2PTunnelPa
         return MODELS.getModel(this.isPowered(), this.isActive());
     }
 
-    private class InputPacketHandler implements IWiredElement, IPacketReceiver {
+    private class InputPacketHandler implements IPeripheral, IPacketSender {
+        @NotNull
+        @Override
+        public Level getLevel() {
+            return ModemP2PTunnelPart.this.getLevel();
+        }
+
+        @NotNull
+        @Override
+        public Vec3 getPosition() {
+            var pos = getHost().getLocation().getPos();
+            return new Vec3(pos.getX(), pos.getY(), pos.getZ());
+        }
+
+        @NotNull
+        @Override
+        public String getSenderID() {
+            return null; // TODO
+        }
+
+        @NotNull
+        @Override
+        public String getType() {
+            return "modem";
+        }
+
+        @Override
+        public boolean equals(@Nullable IPeripheral other) {
+            return false; // TODO
+        }
+    }
+
+    private class OutputPacketHandler implements IPeripheral, IPacketReceiver {
+        @NotNull
+        @Override
+        public Level getLevel() {
+            return ModemP2PTunnelPart.this.getLevel();
+        }
+
+        @NotNull
+        @Override
+        public Vec3 getPosition() {
+            var pos = getHost().getLocation().getPos();
+            return new Vec3(pos.getX(), pos.getY(), pos.getZ());
+        }
+
         @Override
         public double getRange() {
-            return Integer.MAX_VALUE;
+            return Double.MAX_VALUE;
         }
 
         @Override
@@ -65,54 +112,26 @@ public class ModemP2PTunnelPart extends CapabilityP2PTunnelPart<ModemP2PTunnelPa
 
         @NotNull
         @Override
-        public IWiredNode getNode() {
-            return new WiredNode(this);
-        }
-
-        @NotNull
-        @Override
-        public Level getLevel() {
-            return getHost().getLocation().getLevel();
-        }
-
-        @NotNull
-        @Override
-        public Vec3 getPosition() {
-            var blockPos = getHost().getLocation().getPos();
-            return new Vec3(blockPos.getX(), blockPos.getY(), blockPos.getZ());
-        }
-
-        @NotNull
-        @Override
-        public String getSenderID() {
+        public String getType() {
             return "modem";
+        }
+
+        @Override
+        public boolean equals(@Nullable IPeripheral other) {
+            return false; // TODO
         }
     }
 
-    private class OutputPacketHandler implements IWiredElement {
+    private static class NullPacketHandler implements IPeripheral {
         @NotNull
         @Override
-        public IWiredNode getNode() {
-            return new WiredNode(this);
-        }
-
-        @NotNull
-        @Override
-        public Level getLevel() {
-            return getHost().getLocation().getLevel();
-        }
-
-        @NotNull
-        @Override
-        public Vec3 getPosition() {
-            var blockPos = getHost().getLocation().getPos();
-            return new Vec3(blockPos.getX(), blockPos.getY(), blockPos.getZ());
-        }
-
-        @NotNull
-        @Override
-        public String getSenderID() {
+        public String getType() {
             return "modem";
+        }
+
+        @Override
+        public boolean equals(@Nullable IPeripheral other) {
+            return false;
         }
     }
 }
